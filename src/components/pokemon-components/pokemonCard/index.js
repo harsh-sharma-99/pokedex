39f62d597af320761f84374ba43f0fe74e96f087
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2022-01-06 01:25:30 +0530

    Api calls on cards

--- src/components/pokemon-components/pokemonCard/index.js
+++ src/components/pokemon-components/pokemonCard/index.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import "./styles.scss";
+import axios from "axios";
 
 const PokemonCard = ({ name, url }) => {
   const [pokemonDetails, setPokemonDetails] = useState([]);
@@ -14,11 +15,28 @@ const PokemonCard = ({ name, url }) => {
     fetchIndividualPokemon();
   }, [fetchIndividualPokemon]);
 
-  console.log(pokemonDetails);
+  const showAbilities = () => {
+    return pokemonDetails?.abilities?.map((a, idx) => (
+      <li>{a.ability.name} </li>
+    ));
+  };
+
+  const showPhotos = () => {
+    return (
+      <img
+        style={{ width: "212px", height: "240px" }}
+        src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonDetails?.id}.svg`}
+        alt={`${pokemonDetails.name}`}
+      />
+    );
+  };
+
+  // console.log(pokemonDetails);
   return (
     <div className="poke-card">
       <div className="poke-header">{name}</div>
-      {/* <div>{pokemonDetails?.abilities}</div> */}
+      {pokemonDetails && showAbilities()}
+      {pokemonDetails && showPhotos()}
     </div>
   );
 };
